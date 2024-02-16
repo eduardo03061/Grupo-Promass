b08bf54003b21b78a169d44d1dda1008263ffcdd
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2024-02-16 14:44:11 -0600

    Update app.module.ts

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -9,7 +9,7 @@ import { Ticket } from './entities/ticket.entity';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST || 'localhost',
-      port: process.env.DB_PORT || 5432,
+      port: Number(process.env.DB_PORT) || 5432,
       username: process.env.DB_USERNAME || 'postgres',
       password: process.env.DB_PASSWORD || '123456',
       database: process.env.DB_NAME || 'hexalud',

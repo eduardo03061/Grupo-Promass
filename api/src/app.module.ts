d9659588a93234ece0c5d1997ef46441472a2bfd
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2024-02-16 14:43:07 -0600

    feat: config db

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TicketController } from './controllers/ticket.controller';
+import { Ticket } from './entities/ticket.entity';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || '123456',
+      database: process.env.DB_NAME || 'hexalud',
+      entities: [Ticket],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Ticket]),
+  ],
+  controllers: [TicketController],
 })
-export class AppModule {}
+export class AppModule {
+}

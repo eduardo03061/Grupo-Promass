c24ecb831bc77b3e9622e4b7f6222d2ecf1bc5bc
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2024-02-16 20:20:44 -0600

    wip: api

--- api/src/controllers/ticket.controller.ts
+++ api/src/controllers/ticket.controller.ts
@@ -1,27 +1,39 @@
-import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Ticket } from '../entities/ticket.entity';
+import { Like } from 'typeorm';
 
 @Controller('tickets')
 export class TicketController {
   constructor(@InjectRepository(Ticket)
-    private readonly ticketRepository: Repository<Ticket>,
+              private readonly ticketRepository: Repository<Ticket>,
   ) {
   }
 
 
   @Get()
-  async findAllTickets(): Promise<Ticket []> {
-    return await this.ticketRepository.find();
+  async findAllTickets(@Query() data): Promise<Ticket []> {
+    const { query } = data;
+    if (!query) {
+      return await this.ticketRepository.find();
+    }
+
+    return await this.ticketRepository.find(
+      {
+        where:
+          [{ title: Like(`%${query}%`) },
+            { content: Like(`%${query}%`) },
+            { author: Like(`%${query}%`) }],
+      });
   }
 
   @Post()
-  async createTicket(@Body() ticket: Ticket):Promise<Ticket> {
+  async createTicket(@Body() ticket: Ticket): Promise<Ticket> {
     try {
       return await this.ticketRepository.save(ticket);
-    }catch (e) {
-      return e
+    } catch (e) {
+      return e;
     }
 
   }
